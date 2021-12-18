929501095ba36163deba1635876ffa1c595028a3
Jimmy <jimmy881122naruto@gmail.com>
2021-12-19 00:55:44 +0800

    bugs

--- div.v
+++ div.v
@@ -14,33 +14,32 @@ module div (
 );
 
 wire o_out_valid_res;
-wire [50:0] numbers [0:8];
-wire [7:0] intmdt_r [5:0];
+wire [50:0] numbers [0:12];
+wire [4:0] intmdt_r [7:0];
 wire [7:0] o_q_res;
 wire inv_o_q_res_0;
-wire [1:0] tmp;
 
-STAGE1 stage1(i_a[7], i_b, o_q[7], intmdt_r[7][0], numbers[0]);
+STAGE1 stage1(i_a[7], i_b, o_q_res[7], intmdt_r[7][0], numbers[0]);
 STAGE2 stage2({intmdt_r[7][0], i_a[6]}, i_b, o_q_res[6], intmdt_r[6][1:0], numbers[1]);
 STAGE3 stage3({intmdt_r[6][1:0], i_a[5]}, i_b, o_q_res[5], intmdt_r[5][2:0], numbers[2]);
 STAGE4 stage4({intmdt_r[5][2:0], i_a[4]}, i_b, o_q_res[4], intmdt_r[4][3:0], numbers[3]);
-STAGE5 stage5({intmdt_r[4][3:0], i_a[3]}, i_b, o_q_res[3], intmdt_r[3][4:0], numbers[4]);
-STAGE678 stage6({intmdt_r[3][4:0], i_a[2]}, i_b, o_q_res[2], intmdt_r[2], numbers[5]);
-STAGE678 stage7({intmdt_r[2][4:0], i_a[1]}, i_b, o_q_res[1], intmdt_r[1], numbers[6]);
-STAGE678 stage8({intmdt_r[1][4:0], i_a[0]}, i_b, o_q_res[0], intmdt_r[0], numbers[7]);
+STAGE5 stage5({intmdt_r[4][3:0], i_a[3]}, i_b, o_q_res[3], intmdt_r[3], numbers[4]);
+STAGE678 stage6({intmdt_r[3], i_a[2]}, i_b, o_q_res[2], intmdt_r[2], numbers[5]);
+STAGE678 stage7({intmdt_r[2], i_a[1]}, i_b, o_q_res[1], intmdt_r[1], numbers[6]);
+STAGE678 stage8({intmdt_r[1], i_a[0]}, i_b, o_q_res[0], intmdt_r[0], numbers[7]);
 
 IV iv1(inv_o_q_res_0, o_q_res[0], numbers[8]);
-ND2 nd1(tmp[0], o_q_res[0], i_in_valid);
-ND2 nd2(tmp[1], inv_o_q_res_0, i_in_valid);
-ND2 nd2(o_out_valid_res, tmp[0], tmp[1]);
+OR2 nd1(o_out_valid_res, inv_o_q_res_0, o_q_res[0], numbers[9]);
 
-REGP#(14) result(clk, rst_n, {o_q, intmdt_r[0], o_out_valid_res}, {o_q, o_r, o_out_valid}, number);
+REGP#(8) quotient(clk, rst_n, o_q, o_q_res, numbers[10]);
+REGP#(5) remainder(clk, rst_n, o_r, intmdt_r[0], numbers[11]);
+REGP#(1) valid(clk, rst_n, o_out_valid, o_out_valid_res, numbers[12]);
 
 reg [50:0] sum;
 integer j;
 always @(*) begin
 	sum = 0;
-	for (j=0; j<8; j=j+1) begin 
+	for (j=0; j<13; j=j+1) begin 
 		sum = sum + numbers[j];
 	end
 end
@@ -55,27 +54,26 @@ endmodule
 module STAGE1 (
 	input i_a,
 	input [4:0] i_b,
-	output o_q;
+	output o_q,
 	output o_r,
 	output [50:0] number
 );
 
-wire [50:0] numbers [0:5];
+wire [50:0] numbers [0:4];
 
-wire mux_ctrl, inv_o_nor, o_nor, diff, borrow;
+wire inv_o_nor, o_nor, diff, borrow;
 
 HS1 hs1(i_a, i_b[0], diff, borrow, numbers[0]);
 NR4 nr1(o_nor, i_b[4], i_b[3], i_b[2], i_b[1], numbers[1]);
 IV iv1(inv_o_nor, o_nor, numbers[2]);
-NR2 nr2(mux_ctrl, inv_o_nor, borrow, numbers[3]);
-MUX21H mux1(o_r, i_a, diff, mux_ctrl, numbers[4]);
-IV iv2(o_q, borrow, numbers[5]);
+NR2 nr2(o_q, inv_o_nor, borrow, numbers[3]);
+MUX21H mux1(o_r, i_a, diff, o_q, numbers[4]);
 
 reg [50:0] sum;
 integer j;
 always @(*) begin
 	sum = 0;
-	for (j=0; j<6; j=j+1) begin 
+	for (j=0; j<5; j=j+1) begin 
 		sum = sum + numbers[j];
 	end
 end
@@ -90,26 +88,25 @@ endmodule
 module STAGE2 (
 	input [1:0] i_a,
 	input [4:0] i_b,
-	output o_q;
+	output o_q,
 	output [1:0] o_r,
 	output [50:0] number
 );
 
-wire [50:0] numbers [0:3];
+wire [50:0] numbers [0:2];
 
-wire mux_ctrl, borrow;
+wire borrow;
 wire [1:0] diff;
 
 FSP#(2) fs1(i_a, i_b[1:0], diff, borrow, numbers[0]);
-NR4 nr1(mux_ctrl, borrow, i_b[4], i_b[3], i_b[2], numbers[1]);
-MUXP#(2) mux1(o_r, i_a, diff, mux_ctrl, numbers[2]);
-IV iv1(o_q, borrow, numbers[3]);
+NR4 nr1(o_q, borrow, i_b[4], i_b[3], i_b[2], numbers[1]);
+MUXP#(2) mux1(o_r, i_a, diff, o_q, numbers[2]);
 
 reg [50:0] sum;
 integer j;
 always @(*) begin
 	sum = 0;
-	for (j=0; j<4; j=j+1) begin 
+	for (j=0; j<3; j=j+1) begin 
 		sum = sum + numbers[j];
 	end
 end
@@ -124,26 +121,25 @@ endmodule
 module STAGE3 (
 	input [2:0] i_a,
 	input [4:0] i_b,
-	output o_q;
+	output o_q,
 	output [2:0] o_r,
 	output [50:0] number
 );
 
-wire [50:0] numbers [0:3];
+wire [50:0] numbers [0:2];
 
-wire mux_ctrl, diff, borrow;
+wire borrow;
 wire [2:0] diff;
 
 FSP#(3) fs1(i_a, i_b[2:0], diff, borrow, numbers[0]);
-NR3 nr1(mux_ctrl, borrow, i_b[4], i_b[3], numbers[1]);
-MUXP#(3) mux1(o_r, i_a, diff, mux_ctrl, numbers[2]);
-IV iv1(o_q, borrow, numbers[3]);
+NR3 nr1(o_q, borrow, i_b[4], i_b[3], numbers[1]);
+MUXP#(3) mux1(o_r, i_a, diff, o_q, numbers[2]);
 
 reg [50:0] sum;
 integer j;
 always @(*) begin
 	sum = 0;
-	for (j=0; j<4; j=j+1) begin 
+	for (j=0; j<3; j=j+1) begin 
 		sum = sum + numbers[j];
 	end
 end
@@ -158,26 +154,25 @@ endmodule
 module STAGE4 (
 	input [3:0] i_a,
 	input [4:0] i_b,
-	output o_q;
+	output o_q,
 	output [3:0] o_r,
 	output [50:0] number
 );
 
-wire [50:0] numbers [0:3];
+wire [50:0] numbers [0:2];
 
-wire mux_ctrl, diff, borrow;
+wire borrow;
 wire [3:0] diff;
 
 FSP#(4) fs1(i_a, i_b[3:0], diff, borrow, numbers[0]);
-NR2 nr1(mux_ctrl, borrow, i_b[4], numbers[1]);
-MUXP#(4) mux1(o_r, i_a, diff, mux_ctrl, numbers[2]);
-IV iv1(o_q, borrow, numbers[3]);
+NR2 nr1(o_q, borrow, i_b[4], numbers[1]);
+MUXP#(4) mux1(o_r, i_a, diff, o_q, numbers[2]);
 
 reg [50:0] sum;
 integer j;
 always @(*) begin
 	sum = 0;
-	for (j=0; j<4; j=j+1) begin 
+	for (j=0; j<3; j=j+1) begin 
 		sum = sum + numbers[j];
 	end
 end
@@ -192,18 +187,18 @@ endmodule
 module STAGE5 (
 	input [4:0] i_a,
 	input [4:0] i_b,
-	output o_q;
+	output o_q,
 	output [4:0] o_r,
 	output [50:0] number
 );
 
 wire [50:0] numbers [0:2];
 
-wire mux_ctrl, diff, borrow;
+wire borrow;
 wire [4:0] diff;
 
-FSP#(5) fs1(i_a, i_b[3:0], diff, borrow, numbers[0]);
-MUXP#(5) mux1(o_r, diff, i_a, mux_ctrl, numbers[1]);
+FSP#(5) fs1(i_a, i_b, diff, borrow, numbers[0]);
+MUXP#(5) mux1(o_r, diff, i_a, borrow, numbers[1]);
 IV iv1(o_q, borrow, numbers[2]);
 
 reg [50:0] sum;
@@ -225,22 +220,20 @@ endmodule
 module STAGE678 (
 	input [5:0] i_a,
 	input [4:0] i_b,
-	output o_q;
+	output o_q,
 	output [4:0] o_r,
 	output [50:0] number
 );
 
 wire [50:0] numbers [0:3];
 
-wire mux_ctrl, diff, borrow, inv_i_a_5;
+wire borrow, inv_i_a_5;
 wire [4:0] diff;
 
 FSP#(5) fs1(i_a[4:0], i_b, diff, borrow, numbers[0]);
 IV iv1(inv_i_a_5, i_a[5], numbers[1]);
-ND2 nd1(mux_ctrl, inv_i_a_5, borrow, numbers[2]);
-MUXP#(5) mux1(o_r, i_a[4:0], diff, mux_ctrl, numbers[3]);
-
-assign o_q = mux_ctrl;
+ND2 nd1(o_q, inv_i_a_5, borrow, numbers[2]);
+MUXP#(5) mux1(o_r, i_a[4:0], diff, o_q, numbers[3]);
 
 reg [50:0] sum;
 integer j;
@@ -299,15 +292,15 @@ module HS1(
 	input i_b,
 	output o_diff,
 	output o_borrow,
-	output [50:0] number,
+	output [50:0] number
 );
 
 wire inv_a;
 wire [50:0] numbers [0:2];
 
-IV inv1(inv_a, i_a, numbers[0]);
-ND2 nd1(o_borrow, inv_a, i_b, numbers[1]);
-EO2 eo1(o_diff, i_a, i_b, numbers[2]);
+IV iv1(inv_a, i_a, numbers[0]);
+AN2 an1(o_borrow, inv_a, i_b, numbers[1]);
+EO eo1(o_diff, i_a, i_b, numbers[2]);
 
 reg [50:0] sum;
 integer j;
@@ -338,7 +331,7 @@ wire inv_a;
 wire [2:0] prod;
 wire [50:0] numbers [0:5];
 
-IV inv1(inv_a, i_a, numbers[0]);
+IV iv1(inv_a, i_a, numbers[0]);
 ND2 nd1(prod[0], inv_a, i_b, numbers[1]);
 ND2 nd2(prod[1], i_b, i_borrow, numbers[2]);
 ND2 nd3(prod[2], inv_a, i_borrow, numbers[3]);
@@ -379,7 +372,7 @@ HS1 hs1(i_a[0], i_b[0], o_diff[0], intmdt_borrow[0], numbers[0]);
 genvar i;
 generate
 	for (i=1; i<BW; i=i+1) begin
-		FS1 fs(i_a[i], i_b[i], o_diff[i], intmdt_borrow[i-1], intmdt_borrow[i], numbers[i]);
+		FS1 fs(i_a[i], i_b[i], intmdt_borrow[i-1], o_diff[i], intmdt_borrow[i], numbers[i]);
 	end
 endgenerate
 
@@ -411,7 +404,6 @@ module MUXP#(
 	output [50:0] number
 );
 
-wire [BW-1:0];
 wire [50:0] numbers [0:BW-1];
 
 genvar i;
